7f2a946e3a27b33335400b39bbfb28f25ee38f65
Amitha Pai <amitha.pai@zapcg.com>
2024-06-25 12:24:23 +0530

    Updated Card Component

--- frontend/src/Components/ServiceCard.jsx
+++ frontend/src/Components/ServiceCard.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
-import './ServiceCard.css'
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import './ServiceCard.css';
 
-export default function ServiceCard({image, title, description}) {
+export default function ServiceCard({ image, title, description, horizontal }) {
   return (
-    <div>
-      <Card >
-      <Card.Img variant="top" src={image} />
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>
-          {description}
-        </Card.Text>
-      </Card.Body>
-    </Card>
+    <div className={`service-card ${horizontal ? 'horizontal' : 'vertical'}`}>
+      <Card>
+        <div className={`card-img-container ${horizontal ? 'horizontal' : 'vertical'}`}>
+          <Card.Img variant="top" src={image} className={horizontal ? 'card-img-horizontal' : 'card-img-vertical'} />
+        </div>
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{description}</Card.Text>
+        </Card.Body>
+      </Card>
     </div>
-  )
+  );
 }
